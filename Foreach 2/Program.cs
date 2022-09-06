d98ebaca4e8fdfae08ce65552b9ce4bad1a6af81
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-06 15:42:36 +0200

    Lagt till Foreach 2

---
+++ Foreach 2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Testa själv 2)
+//Deklarera en array av datatypen string som du tilldelar tre boktitlar.
+//Skriv ut boktitlarna med hjälp av en foreach-loop.
+
+
+string[] books = { "Very good book nr 1", "Not so very good book nr 2,", "Hilarious book nr 3" };
+
+foreach (var book in books)
+{
+    Console.WriteLine(book);
+}
