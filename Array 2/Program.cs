b4a9c459ac3f7f9541eae0e964c70f6b39e2e2c4
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-06 12:57:28 +0200

    Lagt till Array 2

---
+++ Array 2/Program.cs
@@ -0,0 +1,15 @@
+﻿//2.
+//Använd följande kod:
+//int[] numbers = { 12, 50, 72 };
+//Addera talen med hjälp av en for-loop.
+//Skriv ut summan.
+
+int[] numbers = { 12, 50, 72 };
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+
+Console.WriteLine(sum);
