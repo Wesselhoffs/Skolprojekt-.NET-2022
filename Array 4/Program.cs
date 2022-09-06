6750d549c2412325e6d2a5a5154e64a940e3c87d
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-06 13:31:16 +0200

    Lagt till Array 4

---
+++ Array 4/Program.cs
@@ -0,0 +1,11 @@
+﻿//4.
+//Skapa en tvådimensionell array (googla) innehållande boktitel och pris för tre
+//böcker.
+//Skriv ut informationen till användaren med hjälp av en for-loop.
+
+string[,] books = new string[,] { { "Varje dag är en fest", "169" }, { "Musse och Helium, Jakten på guldosten", "119" }, { "Pannkakstårtan", "139" } };
+
+for (int i = 0; i < books.Length / 2; i++)
+{
+    Console.WriteLine($"Boken \"{books[i, 0]}\" kostar: {books[i, 1]}kr.");
+}
