ceea843886b97be3103dd857f59c6b504652b3ea
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-08 15:23:09 +0200

    Quicksave

--- Uppgift 1. It takes Klass/Program.cs
+++ Uppgift 1. It takes Klass/Program.cs
@@ -1,5 +1,31 @@
-﻿class WebsiteGenerator
-{
+﻿WebsiteGenerator myWebsite = new WebsiteGenerator();
 
+myWebsite.PrintStart();
+myWebsite.PrintEnd();
+
+
+class WebsiteGenerator
+{
+    public string start = "<html>\n<body>\n<main>\n";
+    public string end = "</main>\n</body>\n</html>\n";
+
+    public string[] courses = { "C#", "Databaser", "Webutveckling", "Clean Code" };
+
+    public void PrintHeader(string className)
+    {
+        Console.WriteLine($"<h1>Välkomna!: {className}</h1>");
+    }
+
+    
+
+    public void PrintStart()
+    {
+        Console.Write(start);
+    }
+
+    public void PrintEnd()
+    {
+        Console.Write(end);
+    }
 
 }
