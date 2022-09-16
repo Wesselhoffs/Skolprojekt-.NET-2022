b7b8a1adfd527ef4b2b756bf0022a4e1bd211e0e
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-16 09:05:31 +0200

    Ändrat lite grejer osv!

--- Feeling rather abstract/Website.cs
+++ Feeling rather abstract/Website.cs
@@ -12,11 +12,43 @@ namespace Feeling_rather_abstract
         protected string end = "</main>\n</body>\n</html>\n";
         protected string[] techniques, messageToClass;
         protected string className;
+
+        public Website()
+        {
+            this.className = FakeApiGetClassName();
+            this.messageToClass = FakeApiGetMessage();
+            string[] techniquesFromApi = FakeApiGetTechniques();
+            this.techniques = FixArray(techniquesFromApi);
+        }
+        private string FakeApiGetClassName()
+        {
+            return ".NET Distans 2022";
+        }
+        private string[] FakeApiGetMessage()
+        {
+            string[] messages = { "Glöm inte att övning ger färdighet!", "Öppna boken på sida 257." };
+            return messages;
+        }
+        private string[] FakeApiGetTechniques()
+        {
+            string[] techniques = { "   C#", "daTAbaser", "WebbuTVeCkling ", "clean Code   " };
+            return techniques;
+        }
         public abstract void PrintWebsite();
         protected abstract void PrintStart();
         protected abstract void PrintHeader();
         protected abstract void PrintCourses();
         protected abstract void PrintEnd();
+
+        protected string[] FixArray(string[] techniques)
+        {
+            string[] temp = new string[techniques.Length];
+            for (int i = 0; i < techniques.Length; i++)
+            {
+                temp[i] = "<p>Kurs i " + techniques[i].Trim().ToUpper().Substring(0, 1) + techniques[i].Trim().Substring(1).ToLower() + "<p/>";
+            }
+            return temp;
+        }
 
     }
 }
