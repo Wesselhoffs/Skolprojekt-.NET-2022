c3d09114c3e24056aee9b9201ebd249d316f63e5
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-09-21 14:34:11 +0200

    Fixed now!

--- Extension Methods/Program.cs
+++ Extension Methods/Program.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using static Utilities;
 
-Console.WriteLine(HasValue("Does this have value?"));
-Console.WriteLine(HasValue("   ")); // How about this?
+Console.WriteLine("Does this string have value?".HasValue());
+Console.WriteLine("     ".HasValue());
 
-Console.WriteLine(WordCount("bla bla bla"));
+Console.WriteLine("One two three four five six seven eight nine ten".WordCount());
 
-ToLineArray("Rad 1\r\nRad2\r\nRad3\r\nRad4\r\nRad5");
+Console.WriteLine("Row 1\r\nRow 2\r\nRow 3\r\nRow 4".ToLineArray().Count);  // Counts the lines in the returned list
 
-Console.WriteLine("4 is even?" + IsEven(4));
-Console.WriteLine("5 is even?" + IsEven(5));
+Console.WriteLine(5.IsEven());
+Console.WriteLine(4.IsEven());
+
+Console.WriteLine("Can i parse this to int?".IsNumeric());
+Console.WriteLine("12345".IsNumeric()); // Can i parse this?
 
 string userName = "MittUserName", lastLogin = "MittSenasteLogin";
-
 string message = "Welcome {0} (Last login: {1})".FormatString(userName, lastLogin);
 Console.WriteLine(message);
-
 
 static class Utilities
 {
     //Övning 1
     //Skriv en tilläggsmetod, HasValue, till typen string. Den skall returnera true om strängen innehåller något tecken annat än blanksteg
-    public static bool HasValue(string text)
+    public static bool HasValue(this string text)
     {
         if (string.IsNullOrWhiteSpace(text))
         {
@@ -37,7 +38,7 @@ static class Utilities
 
     //Övning 2
     //Skriv en tilläggsmetod, WordCount, till typen string. Den skall returnera hur många ord som finns i strängen.
-    public static int WordCount(string text)
+    public static int WordCount(this string text)
     {
         string[] temp = text.Split(" ").ToArray();
         int count = temp.Length;
@@ -47,7 +48,7 @@ static class Utilities
     //Övning 3
     //Skriv en tilläggsmetod, ToLineArray, till typen string. Den skall returnera en lista av strängar där varje rad i strängen är ett index i listan.
 
-    public static List<string> ToLineArray(string text)
+    public static List<string> ToLineArray(this string text)
     {
         List<string> stringList = new List<string>(text.Split(Environment.NewLine).ToList());
         return stringList;
@@ -56,7 +57,7 @@ static class Utilities
     //Övning 4
     //Skriv en tilläggsmetod, IsEven, till typen int. Den skall returnera true om talet är jämnt.
 
-    public static bool IsEven(int x)
+    public static bool IsEven(this int x)
     {
         if (x % 2 == 0)
             return true;
@@ -67,7 +68,7 @@ static class Utilities
     //Övning 5
     //Skriv en tilläggsmetod, IsNumeric, till typen string. Den skall returnera true om strängen kan göras om till ett heltal.
 
-    public static bool IsNumeric(string text)
+    public static bool IsNumeric(this string text)
     {
         if (int.TryParse(text, out int result)) return true;
 
