b0f31d04e029f401c6f14c7a067ca23c9083fb62
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-10-14 11:41:24 +0200

    Demo Linq Queries

---
+++ Strängar, Linq och Regex/Demo - LINQ queries/Program.cs
@@ -0,0 +1,9 @@
+﻿//Skapa ett nytt projekt av typen ConsoleApp i Visual Studio.
+//Använd följande kod:
+var numbers = new int[] { 99, 33, 22, 51, 71, 81, 32, 52, 62 };
+//Skriv en LINQ query som väljer alla tal som är större än 50.
+//Skriv ut talen med hjälp av en Foreach-loop.
+//Ledtråd:
+//Jämförelseoperator
+
+numbers.Where(n => n > 50).ToList().ForEach(n => Console.WriteLine(n));
