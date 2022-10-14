bbaf8b8167123870b68659047bca88c0168ea293
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-10-14 11:50:08 +0200

    Group By

---
+++ Strängar, Linq och Regex/Demo - LINQ (generics, orderby och group by)/Program.cs
@@ -0,0 +1,25 @@
+﻿var agents = new Agent[] {
+new Agent("Smith", "M100"),
+new Agent("Williams", "M200"),
+new Agent("Smith", "M300"),
+new Agent("Anderson", "M400"),
+new Agent("Jones", "M500"),
+new Agent("Smith", "M700"),
+new Agent("Johnsson", "M700"),
+new Agent("Jones", "M600")
+};var agentQuery = from agent in agents                 group agent by agent.Name;foreach (var agentGroup in agentQuery)
+{
+    foreach (var agent in agentGroup)
+    {
+        Console.WriteLine("Name: " + agent.Name + "\tCode: " + agent.Code);
+    }
+}public class Agent
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+    public Agent(string name, string code)
+    {
+        Name = name;
+        Code = code;
+    }
+}
