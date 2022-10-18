e2365a173c6eeecbb1cf5e4e6c1c3c13e1f3beb7
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-10-18 14:55:42 +0200

    Async

---
+++ Asynkron programmering/Program.cs
@@ -0,0 +1,23 @@
+﻿ThreeSecondMethod();
+TenSecondMethod();
+
+Console.ReadKey();
+
+
+
+
+async void ThreeSecondMethod()
+{
+    Console.WriteLine("Starting 3 second method.");
+    await Task.Delay(3000);
+    Console.WriteLine("3 second method finished.");
+}
+
+
+
+async void TenSecondMethod()
+{
+    Console.WriteLine("Starting 10 second method.");
+    await Task.Delay(10000);
+    Console.WriteLine("10 second method finished.");
+}
