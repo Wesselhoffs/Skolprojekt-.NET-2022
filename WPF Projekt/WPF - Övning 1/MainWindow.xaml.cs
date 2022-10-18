ed9b55b719702c6be8cfa5ec22f43ec58040e583
Wesselhoffs <magnusson.seb.sm@gmail.com>
2022-10-18 15:41:59 +0200

    Övning 1 WPF

---
+++ WPF Projekt/WPF - Övning 1/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace WPF___Övning_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void myOnlyButton_Click(object sender, RoutedEventArgs e)
+        {
+            string leftTemp = myLeftTextBox.Text;
+            string rightTemp = myRightTextBox.Text;
+            myLeftTextBox.Clear();
+            myRightTextBox.Clear();
+            myLeftTextBox.Text = rightTemp;
+            myRightTextBox.Text = leftTemp;
+        }
+    }
+}
